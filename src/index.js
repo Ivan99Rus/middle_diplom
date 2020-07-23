783f894156aff899ed61bbe8751fd0cc93d34955
Ivan99Rus <ivananapa@mail.ru>
2020-07-23 10:54:44 +0400

    feat: add main slider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import dropdownMenu from './modules/dropdownMenu';
 import signUpFreeSession from './modules/signUpFreeSession';
 import callbackForm from './modules/callbackForm';
+import mainSlider from './modules/mainSlider';
 
 dropdownMenu();
 signUpFreeSession();
 callbackForm();
+mainSlider();
