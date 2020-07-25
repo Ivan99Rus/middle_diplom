3dbf1900ca9bc6b7a77512c4dc643e977b23fb7a
Ivan99Rus <ivananapa@mail.ru>
2020-07-25 11:52:03 +0400

    feat: add fixed menu

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ import gift from './modules/gift';
 import mainSlider from './modules/mainSlider';
 import sendForm from './modules/sendForm';
 import validForm from './modules/validForm';
+import fixedMenu from './modules/fixedMenu';
 
 dropdownMenu();
 signUpFreeSession();
 callbackForm();
 gift();
 mainSlider();
-//sendForm(document.getElementById('callback_form'));
 sendForm(document.getElementById('form1'));
 sendForm(document.getElementById('form2'));
 sendForm(document.getElementById('banner-form'));
 validForm('#form1', '#callback_form1-phone');
 validForm('#form2', '#callback_form2-phone');
 validForm('#banner-form', '#phone');
+fixedMenu();
